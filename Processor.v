1eea38d8e1fe87e5f0d2d23a96e7fe12c9482342
gihanchanaka <gihanchanaka@gmail.com>
2017-08-27 14:26:03 +0530

    Processor 75% done

--- Processor.v
+++ Processor.v
@@ -21,8 +21,9 @@ module alu(Result,DATA1,DATA2,Select);
 	
 endmodule
 
+ 
 
-module reg_file(IN,OUT1,OUT2,clk,RESET,INaddr,OUT1addr,OUT2addr);
+module regfile8x8a(clk,INaddr,IN,OUT1addr,OUT1,OUT2addr,OUT2);
 	reg [(8*8)-1:0] memory;
 	input [7:0] IN;
 	output [7:0] OUT1;
@@ -30,7 +31,6 @@ module reg_file(IN,OUT1,OUT2,clk,RESET,INaddr,OUT1addr,OUT2addr);
 	reg [7:0] outReg1;
 	reg [7:0] outReg2;
 	input clk;
-	input RESET;
 	input [2:0] INaddr;
 	input [2:0] OUT1addr;
 	input [2:0] OUT2addr;
@@ -50,46 +50,173 @@ module reg_file(IN,OUT1,OUT2,clk,RESET,INaddr,OUT1addr,OUT2addr);
 		for(i=0;i<8;i++)begin
 			memory[INaddr*8 + i]=IN[i];
 		end
+		
 	end
 
-	always @(posedge RESET) begin
-		for(i=0;i<64;i++)
-			memory[i]=0;
+	
+
+
+endmodule
+
+
+
+module counter (clk, reset,Read_addr);
+	input clk;
+	input reset;
+	output [31:0] Read_addr;
+	 // The outputs are defined as registers too
+	reg [31:0] Read_addr;
+	// The counter doesn't have any delay since the
+	// output is latched when the negedge of the clock happens.
+	
+	always @(negedge clk)begin
+		Read_addr=Read_addr+4;
+	end
+	
+
+endmodule
+
+
+module Instruction_reg (clk, Read_Addr, instruction);
+	reg [8*8 -1:0] instructionMemory;
+	initial
+	begin
+		instructionMemory[8*0+ 7: 8*0+  0]= 8'b00000000;
+		instructionMemory[8*1+ 7: 8*1+  0]= 8'b00000000;
+		instructionMemory[8*2+ 7: 8*2+  0]= 8'b00000000;
+		instructionMemory[8*3+ 7: 8*3+  0]= 8'b00000000;
+		instructionMemory[8*4+ 7: 8*4+  0]= 8'b00000000;
+		instructionMemory[8*5+ 7: 8*5+  0]= 8'b00000000;
+		instructionMemory[8*6+ 7: 8*6+  0]= 8'b00000000;
+		instructionMemory[8*7+ 7: 8*7+  0]= 8'b00000000;	
+	end
+	input clk;
+	input [31:0] Read_Addr;
+	output [31:0] instruction;
+	reg [31:0] instruction;
+	integer i;
+	always @(negedge clk) begin
+		for(i=0;i<8;i++)begin
+			instruction[i]=instructionMemory[Read_Addr+i];
 		end
 	end
 endmodule
 
+module CU(instruction, OUT1addr, OUT2addr, INaddr,SELECT,imValue,imValueMUXControlSignal,addSumMUXControlSignal);
+	//In out here does not make sense for th control unit
+	//They are named in the register file perspective
+	input [31:0] instruction;
+	output [2:0] OUT1addr;
+	output [2:0] OUT2addr;
+	output [2:0] INaddr;
+	output [2:0] SELECT;
+	output [7:0] imValue;
+	output imValueMUXControlSignal;
+	output addSumMUXControlSignal;
+	reg [2:0] OUT1addr;
+	reg [2:0] OUT2addr;
+	reg [2:0] INaddr;
+	reg [2:0] SELECT;
+	reg [2:0] imValue;
+	reg imValueMUXControlSignal;
+	reg addSumMUXControlSignal;
+
+	always @(instruction) begin
+		case(instruction[3:1])
+			3'b000: imValueMUXControlSignal = ~instruction[24];
+			3'b001: addSumMUXControlSignal = instruction[24];
+		endcase
 
 
-module programCounter(OUT,NEXT,clk);
-	output [8:0] OUT;
-	input [8:0] NEXT;
-	input clk;
-	reg [8:0] pc;
-	assign OUT = pc;
-	always @(posedge clk) begin
-		pc=NEXT;
+
+		OUT1addr=instruction[2:0];
+		OUT2addr=instruction[10:8];
+		INaddr=instruction[18:16];
+		imValue=instruction[7:0];
+		SELECT=instruction[27:25];
+
+	end
+endmodule
+
+module MUX(out,a,b,control);
+	input [7:0] a;
+	input [7:0] b;
+	output [7:0] out;
+	input control;
+	reg [7:0] out;
+
+	always@(a,b,control)begin
+		case(control)
+			1'b0 : out=a;
+			1'b1 : out=b;
+		endcase
+	end
+
+endmodule
+
+module TwosComplement(out,in);
+	input [7:0] in;
+	output [7:0] out;
+	reg [7:0] out;
+
+	always @(in)begin
+		out= -1*in;
 	end
 endmodule
 
 
-module instructionRegister(instruction,clk)
+module processor(clk,reset);
+	input clk;
+	input reset;
+
+	wire [31:0] instructionAddress;
+	wire [31:0] instruction;
+
+	counter myCounter(clk,reset,instructionAddress);
+	Instruction_reg myInstruction_reg(clk, instructionAddress, instruction);
+
+
+	wire [2:0] OUT1addr;
+	wire [2:0] OUT2addr;
+	wire [2:0] INaddr;
+	wire [2:0] SELECT;
+	wire [7:0] imValue;
+	wire imValueMUXControlSignal;
+	wire addSumMUXControlSignal;
+
+	CU myCU(instruction,OUT1addr,OUT2addr,INaddr,SELECT,imValue,imValueMUXControlSignal,addSumMUXControlSignal);
+
+	wire [7:0] IN;
+	wire [7:0] OUT1;
+	wire [7:0] OUT2;
+
+	regfile8x8a myregfile8x8a(clk,INaddr,IN,OUT1addr,OUT1,OUT2addr,OUT2);
+
+	wire [7:0] DATA1;
+	wire [7:0] DATA2;
+	wire [7:0] TwosComplementOutput;
+
+	TwosComplement myTwosComplement(TwosComplementOutput,OUT2);
+
+	MUX myMUX_intermediateValue(DATA1,OUT1,imValue,imValueMUXControlSignal);
+	MUX myMUX_addSub(DATA2,OUT2,TwosComplementOutput,addSumMUXControlSignal)
+
+	alu myAlu(IN,DATA1,OUT2,SELECT);//myAlu(Result,DATA1,DATA2,Select)
 
 
 
-module instructionMemory(OUT,address,clk);
 
 endmodule
-	
 
 
 
 module testbed;
-
+/*
 	reg [7:0] DATA1,DATA2;
 	reg [2:0] Select;
 	wire [7:0] Result;
 	alu MyAlu(Result,DATA1,DATA2,Select);
+
 
 	initial
 	begin
@@ -116,5 +243,5 @@ module testbed;
 		$finish;	
 		
 	end
-
+*/
 endmodule
---
+++ project/base.s
@@ -0,0 +1,280 @@
+@ ARM Assembly example for using scanf("%d") and printf("%d")
+@ Try this with characters (%c)
+
+	.text	@ instruction memory
+	
+	.global main
+
+main:
+	@ stack handling
+	@ push (store) lr to the stack
+	sub	sp, sp, #4
+	str	lr, [sp, #0]
+
+readDims:
+	@allocate stack for input
+	sub	sp, sp, #4
+
+	@scanf to get an integer
+	ldr	r0, =formats
+	mov	r1, sp	
+	bl	scanf	@scanf("%d",sp)
+	@r4 holds the number of rows
+	ldr	r1, [sp,#0]
+	mov r4,r1
+
+	@allocate stack for input
+	sub	sp, sp, #4
+
+	@scanf to get an integer
+	ldr	r0, =formats
+	mov	r1, sp	
+	bl	scanf	@scanf("%d",sp)
+	@r5 holds the number of columns
+	ldr	r1, [sp, #0]
+	mov r5,r1
+
+
+	@allocate stack for input
+	sub	sp, sp, #4
+
+	@scanf to get an integer
+	ldr	r0, =formats
+	mov	r1, sp	
+	bl	scanf	@scanf("%d",sp)
+	@r6 holds the opCode
+	ldr	r1, [sp, #0]
+	mov r6,r1
+
+	@r7 holds the number of elements
+	mul r7,r4,r5
+
+printTotal:
+	mov r1,r7	
+	ldr	r0, =formatElements
+	bl 	printf
+
+	mov r8,#0
+
+loopScan:
+	
+	cmp r7,r8
+	beq loopOut
+	bal scan
+mid:
+	add r8,r8,#1
+	bal loopScan	
+scan:
+	@allocate stack for input
+	sub	sp, sp, #4
+
+	@scanf to get an integer
+	ldr	r0, =formats
+	mov	r1, sp	
+	bl	scanf	@scanf("%d",sp)
+	bal mid
+loopOut:
+	
+
+
+
+
+@printing the original matrix
+
+ldr	r0, =formatOriginal
+	bl 	printf
+
+	mov r8,r4
+	sub r8,r8,#1
+outerLoopOriginal:
+	cmp r8,#0
+	blt loopOut200
+	mov r9,r5
+	sub r9,r9,#1
+	bal printOutOriginal
+mid200:
+innerLoopOriginal:
+	cmp r9,#0
+	blt mid300
+	mul r10,r5,r8
+	add r10,r10,r9
+	mov r2,#4
+	mul r10,r2,r10
+	ldr	r1, [sp,r10]
+	bal printInOriginal
+mid400:
+	sub r9,r9,#1
+	bal innerLoopOriginal
+
+mid300:
+	sub r8,r8,#1
+	bal outerLoopOriginal
+
+printOutOriginal:
+	ldr	r0, =formatOut
+	bl 	printf	
+	bal mid200
+
+printInOriginal:
+	ldr	r0, =formatp
+	bl 	printf	
+	bal mid400
+
+loopOut200:
+
+ldr	r0, =formatOut
+	bl 	printf
+
+
+@invert operation starts here
+
+
+ldr	r0, =formatInvert
+	bl 	printf
+
+mov r8,#0
+
+outerLoopInvert:
+	cmp r4,r8
+	beq loopOut2
+	mov r9,#0
+	bal printOutInvert
+mid2:
+innerLoopInvert:
+	cmp r9,r5
+	beq mid3
+	mul r10,r5,r8
+	add r10,r10,r9
+	mov r2,#4
+	mul r10,r2,r10
+	ldr	r1, [sp,r10]
+	bal printInInvert
+mid4:
+	add r9,r9,#1
+	bal innerLoopInvert
+
+mid3:
+	add r8,r8,#1
+	bal outerLoopInvert
+
+printOutInvert:
+	ldr	r0, =formatOut
+	bl 	printf	
+	bal mid2
+
+printInInvert:
+	ldr	r0, =formatp
+	bl 	printf	
+	bal mid4
+
+loopOut2:
+
+ldr	r0, =formatOut
+	bl 	printf
+
+
+@flip operation starts here
+
+
+ldr	r0, =formatFlip
+	bl 	printf
+
+	mov r8,r4
+	sub r8,r8,#1
+outerLoopFlip:
+	cmp r8,#0
+	blt loopOut20
+	mov r9,#0
+	bal printOutFlip
+mid20:
+innerLoopFlip:
+	cmp r9,r5
+	beq mid30
+	mul r10,r5,r8
+	add r10,r10,r9
+	mov r2,#4
+	mul r10,r2,r10
+	ldr	r1, [sp,r10]
+	bal printInFlip
+mid40:
+	add r9,r9,#1
+	bal innerLoopFlip
+
+mid30:
+	sub r8,r8,#1
+	bal outerLoopFlip
+
+printOutFlip:
+	ldr	r0, =formatOut
+	bl 	printf	
+	bal mid20
+
+printInFlip:
+	ldr	r0, =formatp
+	bl 	printf	
+	bal mid40
+
+
+
+loopOut20:
+
+	ldr	r0, =formatOut
+	bl 	printf
+
+	lsl r7,r7,#2
+	add	sp, sp, r7
+	@copy from stack to register
+	ldr	r1, [sp,#0]
+	
+	@release stack
+	add	sp, sp, #4
+
+	@format for printf
+	ldr	r0, =formatopCode
+	bl 	printf
+
+	@copy from stack to register
+	ldr	r1, [sp,#0]
+	
+	@release stack
+	add	sp, sp, #4
+
+	@format for printf
+	ldr	r0, =formatCol
+	bl 	printf
+
+
+	@copy from stack to register
+	ldr	r1, [sp,#0]
+
+	@release stack
+	add	sp, sp, #4
+
+	@format for printf
+	ldr	r0, =formatRow
+	bl 	printf
+
+	mov r8,#0
+
+
+
+exit: 
+	
+    @ stack handling (pop lr from the stack) and return
+	ldr	lr, [sp, #0]
+	add	sp, sp, #4
+	mov	pc, lr		
+	
+	
+	.data	@ data memory
+formats: .asciz "%d"
+formatp: .asciz " %d "
+formatRow: .asciz "The numberOfRows is %d\n"
+formatCol: .asciz "The numberOfCols is %d\n"
+formatopCode: .asciz "The opCode is %d\n"
+formatCheck: .asciz " check "
+formatOut: .asciz "\n"
+formatElements: .asciz "The numberOfElements is %d\n"
+formatOriginal: .asciz "original"
+formatFlip: .asciz "flip"
+formatInvert: .asciz "invert"
