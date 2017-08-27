c65ce8d0bb34378800e8a15907b84b2401814324
gihanchanaka <gihanchanaka@gmail.com>
2017-08-27 18:51:26 +0530

    Processor done! let the debugging start :P Hiruna get to work!

--- Processor.v
+++ Processor.v
@@ -23,8 +23,9 @@ endmodule
 
  
 
-module regfile8x8a(clk,INaddr,IN,OUT1addr,OUT1,OUT2addr,OUT2);
-	reg [(8*8)-1:0] memory;
+module regfile8x8a(debugPin,clk,INaddr,IN,OUT1addr,OUT1,OUT2addr,OUT2);
+	output [63:0] debugPin;
+	reg [(8*8)-1:0] memory=0;
 	input [7:0] IN;
 	output [7:0] OUT1;
 	output [7:0] OUT2;
@@ -36,8 +37,9 @@ module regfile8x8a(clk,INaddr,IN,OUT1addr,OUT1,OUT2addr,OUT2);
 	input [2:0] OUT2addr;
 	integer i;
 
-	assign OUT1=outReg1;
-	assign OUT2=outReg2;
+	assign OUT1=outReg1[7:0];
+	assign OUT2=outReg2[7:0];
+	assign debugPin = memory[63:0];
 
 	always @(posedge clk) begin
 		for(i=0;i<8;i++)begin
@@ -65,12 +67,12 @@ module counter (clk, reset,Read_addr);
 	input reset;
 	output [31:0] Read_addr;
 	 // The outputs are defined as registers too
-	reg [31:0] Read_addr;
+	reg [31:0] Read_addr=0;
 	// The counter doesn't have any delay since the
 	// output is latched when the negedge of the clock happens.
 	
 	always @(negedge clk)begin
-		Read_addr=Read_addr+4;
+		Read_addr=Read_addr+3'b100;
 	end
 	
 
@@ -78,26 +80,63 @@ endmodule
 
 
 module Instruction_reg (clk, Read_Addr, instruction);
-	reg [8*8 -1:0] instructionMemory;
-	initial
-	begin
-		instructionMemory[8*0+ 7: 8*0+  0]= 8'b00000000;
-		instructionMemory[8*1+ 7: 8*1+  0]= 8'b00000000;
-		instructionMemory[8*2+ 7: 8*2+  0]= 8'b00000000;
-		instructionMemory[8*3+ 7: 8*3+  0]= 8'b00000000;
-		instructionMemory[8*4+ 7: 8*4+  0]= 8'b00000000;
-		instructionMemory[8*5+ 7: 8*5+  0]= 8'b00000000;
-		instructionMemory[8*6+ 7: 8*6+  0]= 8'b00000000;
-		instructionMemory[8*7+ 7: 8*7+  0]= 8'b00000000;	
-	end
+	reg [8*32 -1:0] instructionMemory;
+	integer ins;
 	input clk;
 	input [31:0] Read_Addr;
 	output [31:0] instruction;
 	reg [31:0] instruction;
 	integer i;
+	initial
+	begin
+		for(ins=0;ins<8;ins++)begin
+			instructionMemory[ins]=0;
+		end
+		//Instruction 0
+		instructionMemory[32*0+27 : 32*0+24] =4'b0000;//oppcode
+		instructionMemory[32*0+18 : 32*0+16] =3'b001;//Dest
+		instructionMemory[32*0+10 : 32*0+08] =3'b011;//Source 2
+		instructionMemory[32*0+ 2 : 32*0+ 0] =3'b011;//Source 1
+		//Instruction 1;
+		instructionMemory[32*1 +27 : 32*1+24] =4'b0000;//oppcode
+		instructionMemory[32*1+18 : 32*1+16] =3'b001;//Dest
+		instructionMemory[32*1+10 : 32*1+08] =3'b011;//Source 2
+		instructionMemory[32*1+ 2 : 32*1+ 0] =3'b011;//Source 1
+		//Instruction 2;
+		instructionMemory[32*2 +27 : 32*2+24] =4'b0000;//oppcode
+		instructionMemory[32*2+18 : 32*2+16] =3'b000;//Dest
+		instructionMemory[32*2+10 : 32*2+08] =3'b000;//Source 2
+		instructionMemory[32*2+ 2 : 32*2+ 0] =3'b000;//Source 1
+		//Instruction 3;
+		instructionMemory[32*3 +27 : 32*3+24] =4'b0000;//oppcode
+		instructionMemory[32*3+18 : 32*3+16] =3'b000;//Dest
+		instructionMemory[32*3+10 : 32*3+08] =3'b000;//Source 2
+		instructionMemory[32*3+ 2 : 32*3+ 0] =3'b000;//Source 1
+		//Instruction 4;
+		instructionMemory[32*4 +27 : 32*4+24] =4'b0000;//oppcode
+		instructionMemory[32*4+18 : 32*4+16] =3'b000;//Dest
+		instructionMemory[32*4+10 : 32*4+08] =3'b000;//Source 2
+		instructionMemory[32*4+ 2 : 32*4+ 0] =3'b000;//Source 1
+		//Instruction 5;
+		instructionMemory[32*5 +27 : 32*5+24] =4'b0000;//oppcode
+		instructionMemory[32*5+18 : 32*5+16] =3'b000;//Dest
+		instructionMemory[32*5+10 : 32*5+08] =3'b000;//Source 2
+		instructionMemory[32*5+ 2 : 32*5+ 0] =3'b000;//Source 1
+		//Instruction 6;
+		instructionMemory[32*6 +27 : 32*6+24] =4'b0000;//oppcode
+		instructionMemory[32*6+18 : 32*6+16] =3'b000;//Dest
+		instructionMemory[32*6+10 : 32*6+08] =3'b000;//Source 2
+		instructionMemory[32*6+ 2 : 32*6+ 0] =3'b000;//Source 1
+		//Instruction 7;
+		instructionMemory[32*7 +27 : 32*7+24] =4'b0000;//oppcode
+		instructionMemory[32*7+18 : 32*7+16] =3'b000;//Dest
+		instructionMemory[32*7+10 : 32*7+08] =3'b000;//Source 2
+		instructionMemory[32*7+ 2 : 32*7+ 0] =3'b000;//Source 1
+
+	end
 	always @(negedge clk) begin
 		for(i=0;i<8;i++)begin
-			instruction[i]=instructionMemory[Read_Addr+i];
+			instruction[i]=instructionMemory[Read_Addr*8+i];
 		end
 	end
 endmodule
@@ -117,7 +156,7 @@ module CU(instruction, OUT1addr, OUT2addr, INaddr,SELECT,imValue,imValueMUXContr
 	reg [2:0] OUT2addr;
 	reg [2:0] INaddr;
 	reg [2:0] SELECT;
-	reg [2:0] imValue;
+	reg [7:0] imValue;
 	reg imValueMUXControlSignal;
 	reg addSumMUXControlSignal;
 
@@ -165,9 +204,10 @@ module TwosComplement(out,in);
 endmodule
 
 
-module processor(clk,reset);
+module processor(debugPin,clk,reset);
 	input clk;
 	input reset;
+	output [63:0] debugPin;
 
 	wire [31:0] instructionAddress;
 	wire [31:0] instruction;
@@ -190,7 +230,7 @@ module processor(clk,reset);
 	wire [7:0] OUT1;
 	wire [7:0] OUT2;
 
-	regfile8x8a myregfile8x8a(clk,INaddr,IN,OUT1addr,OUT1,OUT2addr,OUT2);
+	regfile8x8a myregfile8x8a(debugPin,clk,INaddr,IN,OUT1addr,OUT1,OUT2addr,OUT2);
 
 	wire [7:0] DATA1;
 	wire [7:0] DATA2;
@@ -199,7 +239,7 @@ module processor(clk,reset);
 	TwosComplement myTwosComplement(TwosComplementOutput,OUT2);
 
 	MUX myMUX_intermediateValue(DATA1,OUT1,imValue,imValueMUXControlSignal);
-	MUX myMUX_addSub(DATA2,OUT2,TwosComplementOutput,addSumMUXControlSignal)
+	MUX myMUX_addSub(DATA2,OUT2,TwosComplementOutput,addSumMUXControlSignal);
 
 	alu myAlu(IN,DATA1,OUT2,SELECT);//myAlu(Result,DATA1,DATA2,Select)
 
@@ -211,37 +251,42 @@ endmodule
 
 
 module testbed;
-/*
-	reg [7:0] DATA1,DATA2;
-	reg [2:0] Select;
-	wire [7:0] Result;
-	alu MyAlu(Result,DATA1,DATA2,Select);
+
+	reg reset;
+	reg clk;
+	wire [63:0] debugPin;
+
+	processor myProcessor(debugPin,clk,reset);
 
 
 	initial
 	begin
-
+		$monitor("reg0 = %7b",debugPin[7:0]);
+		$monitor("reg1 = %7b",debugPin[15:8]);
+		$monitor("reg2 = %7b",debugPin[23:16]);
 		//$dumpfile("wavedata.vcd");
 	    //$dumpvars(0,testbed);
 
-		assign DATA1=5;
-		assign DATA2=10;
+		reset =1'b0;
 
-		#5 Select=3'b000;
-		#5 $display("Assigned %d to  %d",DATA1,Result);
 
-		#5 Select=3'b001;
-		#5 $display("%d + %d = %d",DATA1,DATA2,Result);
+		#5 clk=1'b0;
+		#5 clk=1'b1;
+		#5 clk=1'b0;
+		#5 clk=1'b1;
+		#5 clk=1'b0;
+		#5 clk=1'b1;
+		#5 clk=1'b0;
+		#5 clk=1'b1;
+		#5 clk=1'b0;
+		#5 clk=1'b1;
+		#5 clk=1'b0;
+		#5 clk=1'b1;
 
-		#5 Select=3'b010;
-		#5 $display("%d & %d = %d",DATA1,DATA2,Result);
-
-		#5 Select=3'b011;
-		#5 $display("%d | %d = %d",DATA1,DATA2,Result);
 
 		
 		$finish;	
 		
 	end
-*/
+
 endmodule
