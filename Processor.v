5f4d053cbab9519a0952ad5447ea89a31d4a693b
gihanchanaka <gihanchanaka@gmail.com>
2017-08-28 16:24:07 +0530

    Completed the processor

--- Processor.v
+++ Processor.v
@@ -1,22 +1,23 @@
 //gihanchanaka@gmail.com
-
+//dddd
 module alu(Result,DATA1,DATA2,Select);
-	input [7:0]DATA1,DATA2;
+	input [7:0] DATA1;
+	input [7:0] DATA2;
 	input [2:0] Select;
-	reg [7:0] out;
 	output [7:0] Result;
+	reg [7:0] Result;
 
 	always@(DATA1,DATA2,Select)
 	begin
 	case(Select)
-		3'b000:  out = DATA1;
-		3'b001:  out = DATA1+DATA2;
-		3'b010:  out = DATA1&DATA2;
-		3'b011:  out = DATA1|DATA2;
+		3'b000:  Result = DATA1;
+		3'b001:  Result = DATA1+DATA2;
+		3'b010:  Result = DATA1&DATA2;
+		3'b011:  Result = DATA1|DATA2;
 
 	endcase
 	end
-	assign Result = out;
+
 
 	
 endmodule
@@ -42,14 +43,14 @@ module regfile8x8a(debugPin,clk,INaddr,IN,OUT1addr,OUT1,OUT2addr,OUT2);
 	assign debugPin = memory[63:0];
 
 	always @(posedge clk) begin
-		for(i=0;i<8;i++)begin
-			outReg1[i]=memory[OUT1addr*8 + i];
-			outReg2[i]=memory[OUT2addr*8 + i];
+		for(i=0;i<8;i=i+1) begin
+			outReg1[i]<=memory[OUT1addr*8 + i];
+			outReg2[i]<=memory[OUT2addr*8 + i];
 		end
 	end
 
 	always @(negedge clk) begin
-		for(i=0;i<8;i++)begin
+		for(i=0;i<8;i=i+1)begin
 			memory[INaddr*8 + i]=IN[i];
 		end
 		
@@ -62,12 +63,14 @@ endmodule
 
 
 
-module counter (clk, reset,Read_addr);
+module counter (debugPin_PC,clk, reset,Read_addr);
 	input clk;
 	input reset;
 	output [31:0] Read_addr;
+	output [31:0] debugPin_PC;
 	 // The outputs are defined as registers too
 	reg [31:0] Read_addr=0;
+	assign debugPin_PC = Read_addr;
 	// The counter doesn't have any delay since the
 	// output is latched when the negedge of the clock happens.
 	
@@ -90,55 +93,56 @@ module Instruction_reg (debugPin_InstructionRegister,clk, Read_Addr, instruction
 	integer i;
 
 	assign debugPin_InstructionRegister[31:0]=instruction[31:0];
+	//assign  debugPin_InstructionRegister[31:24] =Read_Addr[7:0] ;
 	initial
 	begin
-		for(ins=0;ins<32;ins++)begin
+		for(ins=0;ins<8*32;ins=ins+1)begin
 			instructionMemory[ins]=1'b0;
 		end
 		//Instruction 0
 		instructionMemory[32*0+27 : 32*0+24] =4'b0000;//oppcode
-		instructionMemory[32*0+18 : 32*0+16] =3'b001;//Dest
-		instructionMemory[32*0+10 : 32*0+08] =3'b011;//Source 2
-		instructionMemory[32*0+ 2 : 32*0+ 0] =3'b011;//Source 1
+		instructionMemory[32*0+18 : 32*0+16] =3'b100;//Dest
+		instructionMemory[32*0+10 : 32*0+08] =3'b000;//Source 2
+		instructionMemory[32*0+ 7 : 32*0+ 0] =8'hFF;//Source 1
 		//Instruction 1;
 		instructionMemory[32*1 +27 : 32*1+24] =4'b0000;//oppcode
-		instructionMemory[32*1+18 : 32*1+16] =3'b001;//Dest
-		instructionMemory[32*1+10 : 32*1+08] =3'b011;//Source 2
-		instructionMemory[32*1+ 2 : 32*1+ 0] =3'b011;//Source 1
+		instructionMemory[32*1+18 : 32*1+16] =3'b110;//Dest
+		instructionMemory[32*1+10 : 32*1+08] =3'b000;//Source 2
+		instructionMemory[32*1+ 7 : 32*1+ 0] =8'hAA;//Source 1
 		//Instruction 2;
 		instructionMemory[32*2 +27 : 32*2+24] =4'b0000;//oppcode
-		instructionMemory[32*2+18 : 32*2+16] =3'b000;//Dest
+		instructionMemory[32*2+18 : 32*2+16] =3'b011;//Dest
 		instructionMemory[32*2+10 : 32*2+08] =3'b000;//Source 2
-		instructionMemory[32*2+ 2 : 32*2+ 0] =3'b000;//Source 1
+		instructionMemory[32*2+ 7 : 32*2+ 0] =8'hBB;//Source 1
 		//Instruction 3;
-		instructionMemory[32*3 +27 : 32*3+24] =4'b0000;//oppcode
-		instructionMemory[32*3+18 : 32*3+16] =3'b000;//Dest
-		instructionMemory[32*3+10 : 32*3+08] =3'b000;//Source 2
-		instructionMemory[32*3+ 2 : 32*3+ 0] =3'b000;//Source 1
+		instructionMemory[32*3 +27 : 32*3+24] =4'b0010;//oppcode ERROR
+		instructionMemory[32*3+18 : 32*3+16] =3'b101;//Dest
+		instructionMemory[32*3+10 : 32*3+08] =3'b110;//Source 2
+		instructionMemory[32*3+ 2 : 32*3+ 0] =3'b011;//Source 1
 		//Instruction 4;
-		instructionMemory[32*4 +27 : 32*4+24] =4'b0000;//oppcode
-		instructionMemory[32*4+18 : 32*4+16] =3'b000;//Dest
-		instructionMemory[32*4+10 : 32*4+08] =3'b000;//Source 2
-		instructionMemory[32*4+ 2 : 32*4+ 0] =3'b000;//Source 1
+		instructionMemory[32*4 +27 : 32*4+24] =4'b0100;//oppcode
+		instructionMemory[32*4+18 : 32*4+16] =3'b001;//Dest
+		instructionMemory[32*4+10 : 32*4+08] =3'b100;//Source 2
+		instructionMemory[32*4+ 2 : 32*4+ 0] =3'b101;//Source 1
 		//Instruction 5;
-		instructionMemory[32*5 +27 : 32*5+24] =4'b0000;//oppcode
-		instructionMemory[32*5+18 : 32*5+16] =3'b000;//Dest
-		instructionMemory[32*5+10 : 32*5+08] =3'b000;//Source 2
-		instructionMemory[32*5+ 2 : 32*5+ 0] =3'b000;//Source 1
+		instructionMemory[32*5 +27 : 32*5+24] =4'b0110;//oppcode
+		instructionMemory[32*5+18 : 32*5+16] =3'b010;//Dest
+		instructionMemory[32*5+10 : 32*5+08] =3'b001;//Source 2
+		instructionMemory[32*5+ 2 : 32*5+ 0] =3'b110;//Source 1
 		//Instruction 6;
-		instructionMemory[32*6 +27 : 32*6+24] =4'b0000;//oppcode
-		instructionMemory[32*6+18 : 32*6+16] =3'b000;//Dest
+		instructionMemory[32*6 +27 : 32*6+24] =4'b0001;//oppcode
+		instructionMemory[32*6+18 : 32*6+16] =3'b111;//Dest
 		instructionMemory[32*6+10 : 32*6+08] =3'b000;//Source 2
-		instructionMemory[32*6+ 2 : 32*6+ 0] =3'b000;//Source 1
+		instructionMemory[32*6+ 2 : 32*6+ 0] =3'b010;//Source 1
 		//Instruction 7;
-		instructionMemory[32*7 +27 : 32*7+24] =4'b0000;//oppcode
-		instructionMemory[32*7+18 : 32*7+16] =3'b000;//Dest
-		instructionMemory[32*7+10 : 32*7+08] =3'b000;//Source 2
-		instructionMemory[32*7+ 2 : 32*7+ 0] =3'b000;//Source 1
+		instructionMemory[32*7 +27 : 32*7+24] =4'b0011;//oppcode
+		instructionMemory[32*7+18 : 32*7+16] =3'b100;//Dest
+		instructionMemory[32*7+10 : 32*7+08] =3'b011;//Source 2
+		instructionMemory[32*7+ 2 : 32*7+ 0] =3'b111;//Source 1
 
 	end
-	always @(negedge clk) begin
-		for(i=0;i<32;i++)begin
+	always @(posedge clk) begin
+		for(i=0;i<32;i=i+1)begin
 			instruction[i]=instructionMemory[Read_Addr*8+i];
 		end
 	end
@@ -147,7 +151,7 @@ endmodule
 module CU(debugPinCU,instruction, OUT1addr, OUT2addr, INaddr,SELECT,imValue,imValueMUXControlSignal,addSumMUXControlSignal);
 	//In out here does not make sense for th control unit
 	//They are named in the register file perspective
-	output [7:0] debugPinCU;
+	output [15:0] debugPinCU;
 	input [31:0] instruction;
 	output [2:0] OUT1addr;
 	output [2:0] OUT2addr;
@@ -161,10 +165,12 @@ module CU(debugPinCU,instruction, OUT1addr, OUT2addr, INaddr,SELECT,imValue,imVa
 //	reg [2:0] INaddr;
 //	reg [2:0] SELECT;
 //	reg [7:0] imValue;
-	reg imValueMUXControlSignal;
-	reg addSumMUXControlSignal;
+//	reg imValueMUXControlSignal;
+//	reg addSumMUXControlSignal;
 
 	assign debugPinCU[2:0]=SELECT[2:0];
+	assign debugPinCU[5:3]=OUT1addr;
+	assign debugPinCU[8:6]=OUT2addr ;
 
 
 	assign OUT1addr=instruction[2:0];
@@ -172,17 +178,14 @@ module CU(debugPinCU,instruction, OUT1addr, OUT2addr, INaddr,SELECT,imValue,imVa
 	assign INaddr=instruction[18:16];
 	assign imValue=instruction[7:0];
 	assign SELECT=instruction[27:25];
-
-	always @(*) begin
-		case(instruction[3:1])
-			3'b000: imValueMUXControlSignal = ~instruction[24];
-			3'b001: addSumMUXControlSignal = instruction[24];
-		endcase
+	assign imValueMUXControlSignal = ~instruction[24];
+	assign addSumMUXControlSignal = instruction[24];
 
 
 
 
-	end
+
+	
 endmodule
 
 module MUX(out,a,b,control);
@@ -194,8 +197,8 @@ module MUX(out,a,b,control);
 
 	always@(a,b,control)begin
 		case(control)
-			1'b0 : out=a;
-			1'b1 : out=b;
+			1'b0 : out=a[7:0];
+			1'b1 : out=b[7:0];
 		endcase
 	end
 
@@ -207,22 +210,23 @@ module TwosComplement(out,in);
 	reg [7:0] out;
 
 	always @(in)begin
-		out= -1*in;
+		out= ~in + 8'b00000001;
 	end
 endmodule
 
 
-module processor(debugPin,debugPinCU,debugPin_InstructionRegister,clk,reset);
+module processor(debugPin,debugPinCU,debugPin_InstructionRegister,debugPin_PC,clk,reset);
 	input clk;
 	input reset;
 	output [63:0] debugPin;
-	output [7:0] debugPinCU;
+	output [15:0] debugPinCU;
 	output [31:0] debugPin_InstructionRegister;
+	output [31:0] debugPin_PC;
 
 	wire [31:0] instructionAddress;
 	wire [31:0] instruction;
 
-	counter myCounter(clk,reset,instructionAddress);
+	counter myCounter(debugPin_PC,clk,reset,instructionAddress);
 	Instruction_reg myInstruction_reg(debugPin_InstructionRegister,clk, instructionAddress, instruction);
 
 
@@ -249,7 +253,7 @@ module processor(debugPin,debugPinCU,debugPin_InstructionRegister,clk,reset);
 	TwosComplement myTwosComplement(TwosComplementOutput,OUT2);
 
 	MUX myMUX_intermediateValue(DATA1,OUT1,imValue,imValueMUXControlSignal);
-	MUX myMUX_addSub(DATA2,OUT2,TwosComplementOutput,addSumMUXControlSignal);
+	MUX myMUX_addSub(DATA2,TwosComplementOutput,OUT2,addSumMUXControlSignal);
 
 	alu myAlu(IN,DATA1,OUT2,SELECT);//myAlu(Result,DATA1,DATA2,Select)
 
@@ -265,26 +269,37 @@ module testbed;
 	reg reset;
 	reg clk;
 	wire [63:0] debugPin;
-	wire [7:0] debugPinCU;
+	wire [15:0] debugPinCU;
 	wire [31:0] debugPin_InstructionRegister;
+	wire [31:0] debugPin_PC;
 
-	processor myProcessor(debugPin,debugPinCU,debugPin_InstructionRegister,clk,reset);
+	processor myProcessor(debugPin,debugPinCU,debugPin_InstructionRegister,debugPin_PC,clk,reset);
 
 
 	initial
 	begin
-		$monitor("reg0 = %8b",debugPin[7:0]);
-		$monitor("reg1 = %8b",debugPin[15:8]);
-		$monitor("reg2 = %8b",debugPin[23:16]);
+		$monitor("reg0 =%d, reg1 =%d, reg2 =%d, reg3 =%d, reg4 =%d, reg5 =%d, reg6 =%d, reg7 =%d,",debugPin[7:0],debugPin[15:8],debugPin[23:16],debugPin[31:24],debugPin[39:32],debugPin[47:40],debugPin[55:48],debugPin[63:56]);
 
-		$monitor("CU debug pin %8b",debugPinCU[7:0]);
-		$monitor("The instruction %32b",debugPin_InstructionRegister[31:0]);
+		//$monitor("SELECT=%d Add1=%d Add2=%d",debugPinCU[2:0],debugPinCU[5:3],debugPinCU[8:6]);
+		//$monitor("The instruction:  %8b %8b %8b %8b ",debugPin_InstructionRegister[31:24],debugPin_InstructionRegister[23:16],debugPin_InstructionRegister[15:8],debugPin_InstructionRegister[7:0]);
+		//$monitor("PC =%d",debugPin_PC);
+		//$monitor("Clc=%d",clk);
 		//$dumpfile("wavedata.vcd");
 	    //$dumpvars(0,testbed);
 
 		reset =1'b0;
 
-
+		#5 clk=1'b1;
+		#5 clk=1'b0;
+		#5 clk=1'b1;
+		#5 clk=1'b0;
+		#5 clk=1'b1;
+		#5 clk=1'b0;
+		#5 clk=1'b1;
+		#5 clk=1'b0;
+		#5 clk=1'b1;
+		#5 clk=1'b0;
+		#5 clk=1'b1;
 		#5 clk=1'b0;
 		#5 clk=1'b1;
 		#5 clk=1'b0;
@@ -297,8 +312,8 @@ module testbed;
 		#5 clk=1'b1;
 		#5 clk=1'b0;
 		#5 clk=1'b1;
-
-
+		#5 clk=1'b0;
+		#5 clk=1'b1;
 		
 		$finish;	
 		
